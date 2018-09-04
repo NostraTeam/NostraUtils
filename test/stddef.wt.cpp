bdda5b9cf538b7b2b93547ab7648c93e86cea739
Lukas Reichmann <lukas@reichmann-info.de>
2018-09-04 18:01:18 +0200

    Added test nou.sttdef.wt.

---
+++ test/stddef.wt.cpp
@@ -0,0 +1,16 @@
+
+/**
+File:          test.wt.cpp
+Author:        Lukas Reichmann
+Creation Date: 04.09.2018 (dd.mm.yyyy)
+
+A "dummy" test for stddef. Since stddef only contains macros that are hardly testable this is an empty test
+that may be expanded later with actual test cases. For now, this test always succeeds.
+*/
+
+#include "nostrautils/test.hpp"
+
+int main()
+{
+	return 0;
+}
