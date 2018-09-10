d39a9ce787944a5e2833d0e09abe05f3b25658ba
Lukas Reichmann <lukas@reichmann-info.de>
2018-09-10 18:05:03 +0200

    Started implementing pair.
    The basic functionality of the component is finished, there is however still a linker error that has not been fixed yet.

---
+++ include/nostrautils/pair.hpp
@@ -0,0 +1,477 @@
+#ifndef NOU_PAIR_HPP
+#define NOU_PAIR_HPP
+
+/**
+\file nostrautils/pair.hpp
+
+\author  Lukas Reichmann
+\version 1.1.0.0
+\since   1.1.0.0
+
+\brief
+A component that provides a data structure that can hold two values of different types.
+
+\details
+The main class in this component, <code>nou::Pair</code> behaves somewhat like <code>nou::Tuple</code> that
+stores two values. 
+
+Accessing the values stored in a pair is done through the (public) member variables 
+<code>\link nou::Pair::first first\endlink</code> and <code>\link nou::Pair::second second\endlink</code>. 
+The class also supports structural bindings.
+
+<second>Example:</second>
+\code{.cpp}
+nou::Pair<nou::int32, nou::float32> pair = nou::pair(5, 10.0f);
+
+nou::int32 integer = pair.first; //5
+nou::int32 float = pair.second; //10.0f
+\endcode
+
+For a more detailed example, see \link pair.ex.cpp here\endlink.
+*/
+
+/**
+\example pair.ex.cpp
+
+An example that demonstrates the usage of the pair component.
+*/
+
+#include <tuple>
+#include <type_traits>
+
+namespace nou
+{
+    /**
+    \tparam A
+    The type of the first member variable.
+
+    \tparam B
+    The type of the second member variable.
+
+    \brief
+    A simple class that can store two values of different types.
+
+    \author  Lukas Reichmann
+    \version 1.1.0.0
+    \since   1.1.0.0
+    */
+    template<typename A, typename B>
+    class Pair final
+    {
+    public:
+        using FirstType = A;
+        using SecondType = B;
+
+        /**
+        \brief
+        The first value.
+
+        \details
+        This is equivalent to <code>first</code> and <code>left</code>.
+
+        \author  Lukas Reichmann
+        \version 1.1.0.0
+        \since   1.1.0.0
+        */
+        A first;
+
+        /**
+        \brief
+        The second value.
+
+        \details
+        This is equivalent to <code>second</code> and <code>right</code>.
+
+        \author  Lukas Reichmann
+        \version 1.1.0.0
+        \since   1.1.0.0
+        */
+        B second;
+
+        /**
+        \param first
+        The first value.
+
+        \param second
+        The second value.
+
+        \brief 
+        Constructs a new instance from two l-value references.
+
+        \author  Lukas Reichmann
+        \version 1.1.0.0
+        \since   1.1.0.0
+        */
+        template<typename OA, typename OB>
+        Pair(const OA &first, const OB &second);
+
+        /**
+        \param first
+        The first value.
+
+        \param second
+        The second value.
+
+        \brief
+        Constructs a new instance from two r-value references.
+
+        \author  Lukas Reichmann
+        \version 1.1.0.0
+        \since   1.1.0.0
+        */
+        template<typename OA, typename OB>
+        Pair(OA &&first, OB &&second);
+
+        /**
+        \param other
+        The pair to copy from.
+
+        \brief
+        Constructs a new instance from another pair.
+
+        \author  Lukas Reichmann
+        \version 1.1.0.0
+        \since   1.1.0.0
+        */
+        Pair(const Pair<A, B> &other);
+
+        /**
+        \param other
+        The pair to move from.
+
+        \brief
+        Constructs a new instance from another pair.
+
+        \author  Lukas Reichmann
+        \version 1.1.0.0
+        \since   1.1.0.0
+        */
+        Pair(Pair<A, B> &&other);
+
+        /**
+        \param other
+        The pair to copy from.
+
+        \brief
+        Constructs a new instance from another pair.
+
+        \author  Lukas Reichmann
+        \version 1.1.0.0
+        \since   1.1.0.0
+        */
+        template<typename OA, typename OB>
+        Pair(const Pair<OA, OB> &other);
+
+        /**
+        \param other
+        The pair to move from.
+
+        \brief
+        Constructs a new instance from another pair.
+
+        \author  Lukas Reichmann
+        \version 1.1.0.0
+        \since   1.1.0.0
+        */
+        template<typename OA, typename OB>
+        Pair(Pair<OA, OB> &&other);
+
+        /**
+        \tparam N
+        The index of the value to get. Must be either <code>0</code> or <code>1</code>.
+
+        \brief 
+        Returns the value at the specified index.
+
+        \details
+        This function is mainly to allow structured bindings; the data should be accessed using one of the
+        public member variables.
+        */
+        //template<sizeType N>
+        //decltype(auto) get() const;
+
+        /**
+        \param other
+        The pair to copy from.
+
+        \return
+        A reference to the instance that the operator was called on.
+
+        \brief
+        Copies the values of the passed pair to the calling one.
+
+        \author  Lukas Reichmann
+        \version 1.1.0.0
+        \since   1.1.0.0
+        */
+        Pair& operator = (const Pair<A, B> &other);
+
+        /**
+        \param other
+        The pair to move from.
+
+        \return
+        A reference to the instance that the operator was called on.
+
+        \brief
+        Moves the values of the passed pair to the calling one.
+
+        \author  Lukas Reichmann
+        \version 1.1.0.0
+        \since   1.1.0.0
+        */
+        Pair& operator = (Pair<A, B> &&other);
+
+        /**
+        \param other
+        The pair to copy from.
+
+        \return
+        A reference to the instance that the operator was called on.
+
+        \brief
+        Copies the values of the passed pair to the calling one.
+
+        \author  Lukas Reichmann
+        \version 1.1.0.0
+        \since   1.1.0.0
+        */
+        template<typename OA, typename OB>
+        Pair& operator = (const Pair<OA, OB> &other);
+
+        /**
+        \param other
+        The pair to move from.
+
+        \return
+        A reference to the instance that the operator was called on.
+
+        \brief
+        Moves the values of the passed pair to the calling one.
+
+        \author  Lukas Reichmann
+        \version 1.1.0.0
+        \since   1.1.0.0
+        */
+        template<typename OA, typename OB>
+        Pair& operator = (Pair<OA, OB> &&other);
+    };
+
+    /**
+    \param first
+    The first value.
+
+    \param second
+    The second value.
+
+    \brief
+    Constructs a new <code>nou::Pair</code> instance from two l-value references.
+
+    \details
+    This function can be used to write cleaner code, because it does not require the usage of explicit
+    template parameters.
+
+    \author  Lukas Reichmann
+    \version 1.1.0.0
+    \since   1.1.0.0
+    */
+    template<typename A, typename B>
+    Pair<A, B> pair(const A &first, const B &second);
+
+    ///**
+    //\param first
+    //The first value.
+
+    //\param second
+    //The second value.
+
+    //\brief
+    //Constructs a new <code>nou::Pair</code> instance from one l-value reference and one r-value reference.
+
+    //\details
+    //This function can be used to write cleaner code, because it does not require the usage of explicit
+    //template parameters.
+
+    //\author  Lukas Reichmann
+    //\version 1.1.0.0
+    //\since   1.1.0.0
+    //*/
+    //template<typename A, typename B>
+    //Pair<A, B> pair(const A &first, B &&second);
+
+    ///**
+    //\param first
+    //The first value.
+
+    //\param second
+    //The second value.
+
+    //\brief
+    //Constructs a new <code>nou::Pair</code> instance from one r-value reference and one l-value reference.
+
+    //\details
+    //This function can be used to write cleaner code, because it does not require the usage of explicit
+    //template parameters.
+
+    //\author  Lukas Reichmann
+    //\version 1.1.0.0
+    //\since   1.1.0.0
+    //*/
+    //template<typename A, typename B>
+    //Pair<A, B> pair(A &&first, const B &second);
+
+    /**
+    \param first
+    The first value.
+
+    \param second
+    The second value.
+
+    \brief
+    Constructs a new <code>nou::Pair</code> instance from two r-value references.
+
+    \details
+    This function can be used to write cleaner code, because it does not require the usage of explicit 
+    template parameters.
+
+    \author  Lukas Reichmann
+    \version 1.1.0.0
+    \since   1.1.0.0
+    */
+    template<typename A, typename B>
+    Pair<A, B> pair(A &&first, B &&second);
+
+
+
+
+    template<typename A, typename B>
+    template<typename OA, typename OB>
+    Pair<A, B>::Pair(const OA &first, const OB &second) :
+        first(first),
+        second(second)
+    {}
+
+    template<typename A, typename B>
+    template<typename OA, typename OB>
+    Pair<A, B>::Pair(OA &&first, OB &&second) :
+        first(static_cast<OA&&>(first)),
+        second(static_cast<OB&&>(second))
+    {}
+
+    template<typename A, typename B>
+    Pair<A, B>::Pair(const Pair<A, B> &other) : Pair(other.first, other.second)
+    {}
+
+    template<typename A, typename B>
+    Pair<A, B>::Pair(Pair<A, B> &&other) : Pair(static_cast<A&&>(other.first), static_cast<B&&>(other.second))
+    {}
+
+    template<typename A, typename B>
+    template<typename OA, typename OB>
+    Pair<A, B>::Pair(const Pair<OA, OB> &other) : Pair(other.first, other.second)
+    {}
+
+    template<typename A, typename B>
+    template<typename OA, typename OB>
+    Pair<A, B>::Pair(Pair<OA, OB> &&other) : Pair(static_cast<A&&>(other.first), static_cast<B&&>(other.second))
+    {}
+    /*
+    template<typename A, typename B>
+    template<sizeType N>
+    decltype(auto) Pair<A, B>::get() const
+    {
+        static_assert(N == 0 || N == 1);
+
+        if constexpr (N == 0)
+            return first;
+        else
+            return second;
+    }*/
+    
+    template<typename A, typename B>
+    Pair<A, B>& Pair<A, B>::operator = (const Pair<A, B> &other)
+    {
+        first = other.first;
+        second = other.second;
+
+        return *this;
+    }
+
+    template<typename A, typename B>
+    Pair<A, B>& Pair<A, B>::operator = (Pair<A, B> &&other)
+    {
+        first = static_cast<A&&>(other.first);
+        first = static_cast<B&&>(other.second);
+
+        return *this;
+    }
+
+    template<typename A, typename B>
+    template<typename OA, typename OB>
+    Pair<A, B>& Pair<A, B>::operator = (const Pair<OA, OB> &other)
+    {
+        first = other.first;
+        second = other.second;
+
+        return *this;
+    }
+
+    template<typename A, typename B>
+    template<typename OA, typename OB>
+    Pair<A, B>& Pair<A, B>::operator = (Pair<OA, OB> &&other)
+    {
+        first = static_cast<OA&&>(other.first);
+        second = static_cast<OB&&>(other.second);
+
+        return *this;
+    }
+
+
+
+    template<typename A, typename B>
+    Pair<A, B> pair(const A &first, const B &second)
+    {
+        return Pair<A, B>(first, second);
+    }
+/*
+    template<typename A, typename B>
+    Pair<A, B> pair(const A &first, B &&second)
+    {
+        return Pair<A, B>(first, static_cast<B&&>(second));
+    }
+
+    template<typename A, typename B>
+    Pair<A, B> pair(A &&first, const B &second)
+    {
+        return Pair<A, B>(static_cast<A&&>(first), second);
+    }
+*/
+    template<typename A, typename B>
+    Pair<A, B> pair(A &&first, B &&second)
+    {
+        return Pair<A, B>(static_cast<A&&>(first), static_cast<B&&>(second));
+    }
+}
+/*
+///\cond
+//This exists to enable structured bindings
+namespace std
+{
+    template<typename A, typename B>
+    struct tuple_size<nou::Pair<A, B>> : std::integral_constant<std::size_t, 2> {};
+
+    template<typename A, typename B>
+    struct tuple_element<0, nou::Pair<A, B>>
+    {
+        using type = A;
+    };
+
+    template<typename A, typename B>
+    struct tuple_element<1, nou::Pair<A, B>>
+    {
+        using type = B;
+    };
+}
+///\endcond */
+
+#endif
---
+++ test/pair.wt.cpp
@@ -0,0 +1,249 @@
+
+/**
+File:          pair.wt.cpp
+Author:        Lukas Reichmann
+Creation Date: 08.09.2018 (dd.mm.yyyy)
+
+A test for pair.
+*/
+
+#ifndef NOU_TYPES_HPP
+#include "nostrautils/types.hpp"
+#endif
+
+#ifndef NOU_PAIR_HPP
+#include "nostrautils/pair.hpp"
+#endif
+
+#include <iostream>
+
+#define NOU_TEST_IS_TRUE(...) if(!(__VA_ARGS__)) {std::cout << "Failed in line: " << __LINE__ << "\n"; \
+	exit(__LINE__);}
+
+#define NOU_TEST_IS_EQUAL(a, b) NOU_TEST_IS_TRUE(((a) == (b)))
+
+class Test
+{
+private:
+    nou::int32 m_a;
+
+public:
+    Test(nou::int32 a) : 
+        m_a(a)
+    {}
+
+
+    nou::int32 get() const
+    {
+        return m_a;
+    }
+};
+
+class NoCopy
+{
+public:
+    NoCopy() = default;
+    NoCopy(const NoCopy&) = delete;
+    NoCopy(NoCopy&&) = default;
+};
+
+class NoMove
+{
+public:
+    NoMove() = default;
+    NoMove(const NoMove&) = default;
+    NoMove(NoMove&&) = delete;
+};
+
+int main()
+{
+    nou::Pair<nou::int32, nou::float32> pair = nou::pair(5, 6.0f);
+
+    NOU_TEST_IS_EQUAL(pair.first, 5);
+    NOU_TEST_IS_EQUAL(pair.second, 6.0f);
+
+    //test structured bindings
+    auto[a, b] = pair;
+
+    NOU_TEST_IS_EQUAL(a, pair.first);
+    NOU_TEST_IS_EQUAL(b, pair.second);
+    
+    /*//test nou::pair()
+    {
+        Test test1(5);
+        Test test2(6);
+        nou::Pair<Test, Test> pair1 = nou::pair(test1, test2);
+
+        NOU_TEST_IS_EQUAL(pair1.first.get(), 5);
+        NOU_TEST_IS_EQUAL(pair1.second.get(), 6);
+
+        pair1 = nou::pair(Test(5), test2);
+
+        NOU_TEST_IS_EQUAL(pair1.first.get(), 5);
+        NOU_TEST_IS_EQUAL(pair1.second.get(), 6);
+
+        pair1 = nou::pair(test1, Test(6));
+
+        NOU_TEST_IS_EQUAL(pair1.first.get(), 5);
+        NOU_TEST_IS_EQUAL(pair1.second.get(), 6);
+
+        pair1 = nou::pair(Test(5), Test(6));
+
+        NOU_TEST_IS_EQUAL(pair1.first.get(), 5);
+        NOU_TEST_IS_EQUAL(pair1.second.get(), 6);
+
+    }
+    
+    //test copy constructors
+    {
+        Test test1(5);
+        Test test2(6);
+
+        nou::Pair<Test, Test> pair1(test1, test2);
+        nou::Pair<Test, Test> pair2(pair1);
+        NOU_TEST_IS_EQUAL(pair2.first.get(), 5);
+        NOU_TEST_IS_EQUAL(pair2.second.get(), 6);
+    }
+    {
+        Test test1(5);
+        Test test2(6);
+
+        nou::Pair<Test, Test&> pair1(test1, test2);
+        nou::Pair<Test, Test> pair2(pair1);
+        NOU_TEST_IS_EQUAL(pair2.first.get(), 5);
+        NOU_TEST_IS_EQUAL(pair2.second.get(), 6);
+    }
+    {
+        Test test1(5);
+        Test test2(6);
+
+        nou::Pair<Test&, Test> pair1(test1, test2);
+        nou::Pair<Test, Test> pair2(pair1);
+        NOU_TEST_IS_EQUAL(pair2.first.get(), 5);
+        NOU_TEST_IS_EQUAL(pair2.second.get(), 6);
+    }
+    {
+        Test test1(5);
+        Test test2(6);
+
+        nou::Pair<Test&, Test&> pair1(test1, test2);
+        nou::Pair<Test, Test> pair2(pair1);
+        NOU_TEST_IS_EQUAL(pair2.first.get(), 5);
+        NOU_TEST_IS_EQUAL(pair2.second.get(), 6);
+    }
+    
+    //test move constructors
+    {
+        Test test1(5);
+        Test test2(6);
+
+        nou::Pair<Test, Test> pair2(nou::pair<Test, Test>(test1, test2));
+        NOU_TEST_IS_EQUAL(pair2.first.get(), 5);
+        NOU_TEST_IS_EQUAL(pair2.second.get(), 6);
+    }
+    {
+        Test test1(5);
+        Test test2(6);
+
+        nou::Pair<Test, Test> pair2(nou::pair<Test, Test&>(test1, test2));
+        NOU_TEST_IS_EQUAL(pair2.first.get(), 5);
+        NOU_TEST_IS_EQUAL(pair2.second.get(), 6);
+    }
+    {
+        Test test1(5);
+        Test test2(6);
+
+        nou::Pair<Test, Test> pair2(nou::pair<Test&, Test>(test1, test2));
+        NOU_TEST_IS_EQUAL(pair2.first.get(), 5);
+        NOU_TEST_IS_EQUAL(pair2.second.get(), 6);
+    }
+    {
+        Test test1(5);
+        Test test2(6);
+
+        nou::Pair<Test, Test> pair2(nou::pair<Test&, Test&>(test1, test2));
+        NOU_TEST_IS_EQUAL(pair2.first.get(), 5);
+        NOU_TEST_IS_EQUAL(pair2.second.get(), 6);
+    }*/
+    
+    //test copy assign
+    {
+        Test test1(5);
+        Test test2(6);
+
+        nou::Pair<Test, Test> pair1(test1, test2);
+        nou::Pair<Test, Test> pair2(Test(1000), Test(2000));
+        pair2 = pair1;
+        NOU_TEST_IS_EQUAL(pair2.first.get(), 5);
+        NOU_TEST_IS_EQUAL(pair2.second.get(), 6);
+    }
+    {
+        Test test1(5);
+        Test test2(6);
+        
+        nou::Pair<Test, Test&> pair1(test1, test2);
+        nou::Pair<Test, Test> pair2(pair1);
+        NOU_TEST_IS_EQUAL(pair2.first.get(), 5);
+        //NOU_TEST_IS_EQUAL(pair2.second.get(), 6);
+    }
+    /*{
+        Test test1(5);
+        Test test2(6);
+
+        nou::Pair<Test&, Test> pair1(test1, test2);
+        nou::Pair<Test, Test> pair2(Test(1000), Test(2000));
+        pair2 = pair1;
+        NOU_TEST_IS_EQUAL(pair2.first.get(), 5);
+        NOU_TEST_IS_EQUAL(pair2.second.get(), 6);
+    }
+    /*{
+        Test test1(5);
+        Test test2(6);
+
+        nou::Pair<Test&, Test&> pair1(test1, test2);
+        nou::Pair<Test, Test> pair2(Test(1000), Test(2000));
+        pair2 = pair1;
+        NOU_TEST_IS_EQUAL(pair2.first.get(), 5);
+        NOU_TEST_IS_EQUAL(pair2.second.get(), 6);
+    }
+/*
+    //test move assign
+    {
+        Test test1(5);
+        Test test2(6);
+
+        nou::Pair<Test, Test> pair2(Test(1000), Test(2000));
+        pair2 = nou::pair<Test, Test>(test1, test2);
+        NOU_TEST_IS_EQUAL(pair2.first.get(), 5);
+        NOU_TEST_IS_EQUAL(pair2.second.get(), 6);
+    }
+    {
+        Test test1(5);
+        Test test2(6);
+
+        nou::Pair<Test, Test> pair2(Test(1000), Test(2000));
+        pair2 = nou::pair<Test, Test&>(test1, test2);
+        NOU_TEST_IS_EQUAL(pair2.first.get(), 5);
+        NOU_TEST_IS_EQUAL(pair2.second.get(), 6);
+    }
+    {
+        Test test1(5);
+        Test test2(6);
+
+        nou::Pair<Test, Test> pair2(Test(1000), Test(2000));
+        pair2 = nou::pair<Test&, Test>(test1, test2);
+        NOU_TEST_IS_EQUAL(pair2.first.get(), 5);
+        NOU_TEST_IS_EQUAL(pair2.second.get(), 6);
+    }
+    {
+        Test test1(5);
+        Test test2(6);
+
+        nou::Pair<Test, Test> pair2(Test(1000), Test(2000));
+        pair2 = nou::pair<Test&, Test&>(test1, test2);
+        NOU_TEST_IS_EQUAL(pair2.first.get(), 5);
+        NOU_TEST_IS_EQUAL(pair2.second.get(), 6);
+    }*/
+
+    return 0;
+}
